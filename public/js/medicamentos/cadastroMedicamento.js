815fd68d230c112e6d9b55edd4de2d926f7b2204
Vitor Cesar Lulio <vitorcesarlulio@hotmail.com>
2023-11-01 16:22:09 -0300

    Mensagem de sucesso ou erro ao cadastrar medicamento

--- public/js/medicamentos/cadastroMedicamento.js
+++ public/js/medicamentos/cadastroMedicamento.js
@@ -1,21 +1,36 @@
-$(document).ready(function() {
-    $('#formCadastroMedicamento').on('submit', function(e) {
-      e.preventDefault();
-  
-      $.ajax({
-        url: '/medicamentos',
-        type: 'POST',
-        data: $(this).serialize(),
-        success: function(response) {
-          // Trate a resposta aqui, por exemplo, fechar o modal
-          $('#modalCadastroMedicamento').modal('hide');
-          // Recarregar a lista de medicamentos ou exibir uma mensagem
-        },
-        error: function(error) {
-          // Trate erros aqui
-          alert('Erro ao cadastrar medicamento');
-        }
-      });
+$(document).ready(function () {
+
+  // Configurações padrão para Toastr (opcional)
+  toastr.options = {
+    "closeButton": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  $('#formCadastroMedicamento').on('submit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: '/medicamentos',
+      type: 'POST',
+      data: $(this).serialize(),
+      success: function (response) {
+        $('#modalCadastroMedicamento').modal('hide');
+
+        toastr.success('Medicamento cadastrado com sucesso!');
+
+        // Recarregar a lista de medicamentos ou exibir uma mensagem
+      },
+      error: function (error) {
+        toastr.error('Erro ao cadastrar medicamento.');
+      }
     });
   });
-  
+});
