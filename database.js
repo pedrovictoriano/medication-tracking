839cf218ab09c29a615aa1c296f508520ab1c8a5
Vitor Cesar Lulio <vitorcesarlulio@hotmail.com>
2023-11-03 16:57:45 -0300

    Utilizando .env para variaveis de ambiente

--- database.js
+++ database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
 // Conexão com o banco de dados
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'medication_tracking'
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME
 });
 
 const getCountMedicamentos = (callback) => {
