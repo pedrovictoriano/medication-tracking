6415e8122a1659228a872fa4294cc5b687ac4244
Vitor Cesar Lulio <vitorcesarlulio@hotmail.com>
2023-11-03 17:01:25 -0300

    Separando config do toat

--- public/js/medicamentos/cadastroMedicamento.js
+++ public/js/medicamentos/cadastroMedicamento.js
@@ -1,20 +1,4 @@
 $(document).ready(function () {
-
-  // Configurações padrão para Toastr (opcional)
-  toastr.options = {
-    "closeButton": true,
-    "progressBar": true,
-    "positionClass": "toast-top-right",
-    "showDuration": "300",
-    "hideDuration": "1000",
-    "timeOut": "5000",
-    "extendedTimeOut": "1000",
-    "showEasing": "swing",
-    "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
-  }
-
   $('#formCadastroMedicamento').on('submit', function (e) {
     e.preventDefault();
     $.ajax({
