a3d3b6ee854c2483d339006071f2b9bff2382e7e
Vitor Cesar Lulio <vitorcesarlulio@hotmail.com>
2023-11-29 22:32:06 -0300

    Legeda dias para vencer e retirada header no footer do datables

--- public/js/lotes/lotes.js
+++ public/js/lotes/lotes.js
@@ -1,4 +1,6 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //Datemask dd/mm/yyyy
     $('.data-mask-date').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' });
+
+    $('[data-toggle="tooltip"]').tooltip();
 });
