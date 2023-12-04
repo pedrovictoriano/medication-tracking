ae8c5c6e6e2dfba710c46d2b1ad5c104834b95bc
Vitor Cesar Lulio <vitorcesarlulio@hotmail.com>
2023-12-04 20:18:33 -0300

    Front end itens estoque

--- public/js/estoque/estoque.js
+++ public/js/estoque/estoque.js
@@ -2,23 +2,23 @@ var stepper; // Definindo como global
 
 // Init bs-stepper
 document.addEventListener('DOMContentLoaded', function () {
-    var stepperElement = document.querySelector('.bs-stepper');
-    stepper = new Stepper(stepperElement, {
-        linear: false,
-        animation: true
-    });
+  var stepperElement = document.querySelector('.bs-stepper');
+  stepper = new Stepper(stepperElement, {
+    linear: false,
+    animation: true
+  });
 
-    // Esquema para controlar botões no modal-footer
-    stepperElement.addEventListener('show.bs-stepper', function (event) {
-        var activeStep = event.detail.indexStep;
-        var steps = stepperElement.querySelectorAll('.step');
-        var totalSteps = steps.length - 1; // Ajuste se necessário
+  // Esquema para controlar botões no modal-footer
+  stepperElement.addEventListener('show.bs-stepper', function (event) {
+    var activeStep = event.detail.indexStep;
+    var steps = stepperElement.querySelectorAll('.step');
+    var totalSteps = steps.length - 1; // Ajuste se necessário
 
-        // Controle dos botões
-        document.getElementById('previous-btn').style.display = activeStep > 0 ? 'inline-block' : 'none';
-        document.getElementById('next-btn').style.display = activeStep < totalSteps ? 'inline-block' : 'none';
-        document.getElementById('submit-btn').style.display = activeStep === totalSteps ? 'inline-block' : 'none';
-    });
+    // Controle dos botões
+    document.getElementById('previous-btn').style.display = activeStep > 0 ? 'inline-block' : 'none';
+    document.getElementById('next-btn').style.display = activeStep < totalSteps ? 'inline-block' : 'none';
+    document.getElementById('submit-btn').style.display = activeStep === totalSteps ? 'inline-block' : 'none';
+  });
 });
 
 $(document).ready(function () {
@@ -29,6 +29,37 @@ $(document).ready(function () {
   $.get('/api/tipos_movimentacoes', function (tipos_movimentacoes) {
     tipos_movimentacoes.forEach(function (movimentacao) {
       $('#insertTipoMovimentacao').append(new Option(movimentacao.id + " - " + movimentacao.descricao, movimentacao.id));
+    });
+  });
+
+  $.get('/api/lotes', function (lotes) {
+    lotes.data.forEach(function (lote) {
+      $('#insertLotes').append(new Option(lote.id + " - " + lote.numero_lote, lote.id));
+    });
+  });
+
+
+
+  $.get('/api/medicamentos', function (medicamentos) {
+    medicamentos.data.forEach(medicamento => {
+      $('#insertMedicamento').append(
+        $('<option>', {
+          value: medicamento.medicamento_id,
+          text: medicamento.medicamento_id + " - " + medicamento.medicamento_nome_comercial,
+          'data-apresentacao': medicamento.medicamento_apresentacao
+        })
+      );
+    });
+  });
+
+  $('#insertMedicamento').on('change', function () {
+    var apresentacao = $(this).find('option:selected').data('apresentacao');
+    $('#insertApresetacao').val(apresentacao);
+});
+
+  $.get('/api/localizacoes', function (localizacoes) {
+    localizacoes.forEach(function (localizacao) {
+      $('#insertLocalizacao').append(new Option(localizacao.id + " - " + localizacao.descricao, localizacao.id));
     });
   });
 });
--- public/js/estoque/insertEstoque.js
+++ public/js/estoque/insertEstoque.js
@@ -1,4 +1,3 @@
-
 $("#formEstoquePart").validate({
     rules: {
         documento: {
@@ -39,20 +38,55 @@ $("#formEstoquePart").validate({
     }
 });
 
+$("#formEstoqueItensPart").validate({
+    rules: {
+        lote: {
+            required: true
+        },
+        medicamento: {
+            required: true
+        },
+        quantidade: {
+            required: true
+        },
+        localizacao: {
+            required: true
+        }
+    },
+    messages: {
+        lote: {
+            required: "Selecione um Lote."
+        },
+        medicamento: {
+            required: "Selecione um Medicamento."
+        },
+        quantidade: {
+            required: "Informe uma quantidade."
+        },
+        localizacao: {
+            required: "Selecione uma Localização."
+        }
+    },
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+        error.addClass('invalid-feedback');
+        element.closest('.form-group').append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+        $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).removeClass('is-invalid');
+    },
+    submitHandler: function (form) {
+    }
+});
+
 $('#next-btn').on('click', function () {
     // Valida o formulário manualmente
     if ($("#formEstoquePart").valid()) {
         // Se o formulário for válido, avança para a próxima etapa
         stepper.next();
-        insertEstoque();
-    }
-});
-
-$("#formEstoqueItensPart").validate({
-    // Regras e mensagens para a segunda etapa
-    // ...
-    submitHandler: function (form) {
-        // Executa a ação final, como enviar dados ao servidor
         insertEstoque();
     }
 });
@@ -77,3 +111,85 @@ function insertEstoque() {
         }
     });
 }
+
+// Evento de clique para adicionar item
+$('#btnAdicionarItem').on('click', function () {
+    // Valida o formulário manualmente
+    if ($("#formEstoqueItensPart").valid()) {
+        // Se o formulário for válido
+        inserirItemTable()
+    }
+});
+
+let itensEstoque = []; // Array para armazenar os itens adicionados
+
+function inserirItemTable() {
+    // Coleta os dados do formulário de itens
+    const dadosItem = {
+        lote_id: $('#insertLotes').val(),
+        lote_descricao: $('#insertLotes option:selected').text(), // Descrição do lote
+        medicamento_id: $('#insertMedicamento').val(),
+        medicamento_descricao: $('#insertMedicamento option:selected').text(), // Descrição do medicamento
+        qtd: $('#insertQuantidade').val(),
+        localizacao_id: $('#insertLocalizacao').val(),
+        localizacao_descricao: $('#insertLocalizacao option:selected').text(), // Descrição da localização
+        observacoes: $('#insertItemEstoqueObservacoes').val()
+    };
+
+    // Adiciona o item ao array
+    itensEstoque.push(dadosItem);
+
+    // Limpa o formulário de item
+    $('#formEstoqueItensPart')[0].reset();
+
+    // Limpa e reseta os campos Select2
+    $('#insertLotes').val(null).trigger('change');
+    $('#insertMedicamento').val(null).trigger('change');
+    $('#insertLocalizacao').val(null).trigger('change');
+
+    // Limpa manualmente os campos restantes
+    $('#insertQuantidade').val('');
+    $('#insertItemEstoqueObservacoes').val('');
+
+    // Atualiza a visualização dos itens
+    atualizarListaItens();
+}
+
+function atualizarListaItens() {
+    var tbody = $('#listItensEstoque tbody');
+    tbody.empty();
+    itensEstoque.forEach(function (item, index) {
+        tbody.append(`<tr>
+            <td>${item.lote_descricao}</td>
+            <td>${item.medicamento_descricao}</td>
+            <td>${item.qtd}</td>
+            <td>${item.localizacao_descricao}</td>
+            <td>${item.observacoes}</td>
+            <td><button type="button" class="btn btn-danger btn-sm btn-remover-item" data-index="${index}"><i class="fas fa-trash"></i></button></td>
+        </tr>`);
+    });
+}
+
+// Confirmar remoção de item e realizar remoção
+$(document).on('click', '.btn-remover-item', function () {
+    var index = $(this).data('index'); // Captura o índice do item
+
+    // Mostra uma mensagem de confirmação antes da remoção
+    Swal.fire({
+        title: 'Tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, remover!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            // Se o usuário confirmar, remove o item
+            itensEstoque.splice(index, 1);
+            atualizarListaItens();
+            toastr.success('Item removido com sucesso!');
+        }
+    });
+});
